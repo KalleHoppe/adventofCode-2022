2e69ffccf394635d8cfc5dd1cb0c0226b44d18c2
Kalle Hoppe <kalle.hoppe@gmail.com>
2022-12-06 08:34:19 +0100

    Day 6

--- 6/main.py
+++ 6/main.py
@@ -1,20 +1,18 @@
-
-
 def Part1(signal, markLength):
-   pos = 0
-   packet = []
-   while pos < len(signal):
-      packet.append(signal[pos])
-      if len(set(packet)) == markLength:
-         print(f"The first packet is {packet}")
-         print(f"Chars processed first marker {pos+1}")
-         break
-      if len(packet) == markLength:
-         packet.pop(0)
-      pos +=1
+    pos = 0
+    packet = []
+    while pos < len(signal):
+        packet.append(signal[pos])
+        if len(set(packet)) == markLength:
+            print(f"The first packet is {packet}")
+            print(f"Chars processed first marker {pos+1}")
+            break
+        if len(packet) == markLength:
+            packet.pop(0)
+        pos +=1
 
 if __name__ == "__main__":
-   f = open('/workspaces/adventofCode-2022/6/input.txt')
-   signal = f.read()
-   Part1(signal, 4)
-   Part1(signal, 14)
+    f = open('/workspaces/adventofCode-2022/6/input.txt')
+    signal = f.read()
+    Part1(signal, 4)
+    Part1(signal, 14)
